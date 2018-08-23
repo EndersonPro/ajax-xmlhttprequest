ffe7b8d16378699f0288c843890ccc18d641c2fb
Enderson Vizcaino <enjaviga@gmail.com>
2018-08-23 01:52:56 -0500

    Segundo commit :tada:

--- js/MyScripts.js
+++ js/MyScripts.js
@@ -1,33 +1,40 @@
 /* Capturo el elemto del DOM a traves de su Id */
 const obtener_datos = document.getElementById('obtener_datos');
 
+const data_result = document.getElementById('data-result');
+const info = document.getElementById('info');
+
+const MESSAGEHTML = "<p>Solitud Realizada y Procesada</p>";
+
 obtener_datos.addEventListener('click', function(event){
     /* Llamando a la funcion que realiza mi solicitud AJAX */
     AJAX();
 })
 
 
-function AJAX(uri){
-
+function AJAX(){
     /* Instancia de objeto */
     const xhttp = new XMLHttpRequest;
-
-    /* Defino una UR por defecto a la cual mi metodo hace la solicitud */
-    const defaultURI = 'https://jsonplaceholder.typicode.com/users';
 
     // Definimos el tipo de metodo.
     const METHOD = 'GET';
     
     // URI a la cual se le hara la solicitud
-    const URI = typeof(uri) != 'undefined' ? uri : defaultURI;
+    const URI = 'https://jsonplaceholder.typicode.com/users';
 
-    console.log(URI);
     // Metodo que esta pendiente al cambio de estado
     xhttp.onreadystatechange = function(){
         // Respuesta del servidor.
-        if(xhttp.readyState == 4 && xhttp.status == 200 ){
-            console.log(this.response)
+        if(this.readyState == 4 && this.status == 200 ){
+            const result = JSON.parse(this.response);
+            const view = ResultInView(result);
+            data_result.style.transition = "1s all"
+            data_result.innerHTML = view;
+            info.style.transition = "1s all"
+            info.style.backgroundColor = "#44bd32"
+            info.innerHTML = MESSAGEHTML;
         }
+    
     }
 
     //Metodo que recibe la configuracion de la solicitud.
@@ -36,6 +43,20 @@ function AJAX(uri){
     //Enviar la solicitud.
     xhttp.send()
 
+}
+
+function ResultInView(array){
+    var html = '';
+    array.map((data)=>{
+        html += `
+            <div class="user-info">
+                <p><strong>Nombre: </strong>             ${data.name}        </p>
+                <p><strong>Nombre de usuario: </strong>  ${data.username}    </p>
+                <p><strong>E-mail:</strong>              ${data.email}        </p>
+            </div>
+        `;
+    });
+    return html;
 }
 
 
